3000eea506627c2ee498fa103e9524ac1e317ab1
Seth DeWhitt <seth_dewhitt@outlook.com>
2025-01-02 01:10:16 -0800

    Documentation in mongo.ts

--- evallm/src/app/mongo.ts
+++ evallm/src/app/mongo.ts
@@ -1,5 +1,7 @@
 import { MongoClient, ServerApiVersion } from 'mongodb';
 
+
+// IMPORTANT: Only create one client and do not close every time it is used. Many production errors will happen otherwise
 const client = new MongoClient(process.env.MONGODB_URI as string, {
     serverApi: {
         version: ServerApiVersion.v1,
