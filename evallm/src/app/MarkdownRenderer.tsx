d17d792f950b76cfff173f9270a6585d447f5198
Seth DeWhitt <seth_dewhitt@outlook.com>
2025-01-03 18:12:08 -0800

    react markdown working!!

--- evallm/src/app/MarkdownRenderer.tsx
+++ evallm/src/app/MarkdownRenderer.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
+import './styles.css';
 
 interface MarkdownRendererProps {
     content: string;
 }
-
+// prose prose-stone max-w-none
 const MarkdownRenderer: React.FC<MarkdownRendererProps> = ({ content }) => {
     return (
-        <div className="prose prose-stone max-w-none">
+        <div className="markdown-content max-w-none">
             <ReactMarkdown>{content}</ReactMarkdown>
         </div>
     );
